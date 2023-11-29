9b91c414e75c2890e9ad26f31f51025cd0ede0e9
virgile popote <virgilepopote@gmail.com>
2023-11-29 14:14:44 +0100

    day3after

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(email: "email10@email.fr", password: "azerty1", username: "user10")
-User.create!(email: "email20@email.fr", password: "azerty2", username: "user20")
-User.create!(email: "email30@email.fr", password: "azerty3", username: "user30")
 
-Post.create!(mood: "hope", track_id: "To you", user_id: 1)
-Post.create!(mood: "sad", track_id: "Gather my tears", user_id: 2)
-Post.create!(mood: "Happy", track_id: "Super", user_id: 1)
 
-Comment.create!(content: "Best song ever!!", post_id: 1, user_id: 2)
-Comment.create!(content: ":(((", post_id: 2, user_id: 3)
-Comment.create!(content: "Yeahhhh", post_id: 3, user_id: 1)
+Post.create!(mood: "Tired", track_id: "To you", user_id: 1)
+Post.create!(mood: "Angry", track_id: "Gather my tears", user_id: 1)
+Post.create!(mood: "Excited", track_id: "Super", user_id: 1)
