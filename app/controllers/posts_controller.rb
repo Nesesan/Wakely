452d0fd3c625fe9f0b53a147b52d8404359fe1e3
lounes benzeghioua <lounes.benz@outlook.fr>
2023-12-07 09:40:12 +0100

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to post_path(@post)
   end
 
   def search_music
